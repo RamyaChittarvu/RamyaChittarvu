6c08d72fdab1b2291d4ab3d1b4370e29f84afbab
Ramya Chittarvu <ramya.chittarvu@outlook.com>
2024-08-18 16:35:25 -0400

    DI

--- src/main/java/guru/springframework6/NewApp/springFramework/controllers/MyController.java
+++ src/main/java/guru/springframework6/NewApp/springFramework/controllers/MyController.java
@@ -1,13 +1,22 @@
 package guru.springframework6.NewApp.springFramework.controllers;
 
+import guru.springframework6.NewApp.springFramework.services.MyService;
+import guru.springframework6.NewApp.springFramework.services.MyServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final MyService greetingService;
+
+    public MyController() {
+        this.greetingService = new MyServiceImpl();
+        }
+
     public String sayHello() {
 
         System.out.println("I'm in the controller!");
 
-        return "Hello Everyone!!!";
+        return greetingService.sayGreeting();
     }
 }
